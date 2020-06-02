347b387ef2277ad75ad3fd4778b7d2cbaa16095d
WoojtekP <pawlik.wj@gmail.com>
2020-06-02 13:28:05 +0200

    mast semisplit weighted mean

--- mast_semisplit/src/moves_container.cpp
+++ mast_semisplit/src/moves_container.cpp
@@ -2,19 +2,19 @@
 
 void moves_container::insert_or_update(const reasoner::move& move, const uint& score) {
     for (const auto& action : move.mr) {
-        auto action_index = action.cell * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
+        auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
         map[action_index].first++;
         map[action_index].second += score;
     }
 }
 
 double moves_container::get_score_or_default_value(const reasoner::move& move) {
-    uint size = 0;
-    double total_score = 0.0;
+    uint weight_sum = 0;
+    uint total_score = 0;
     for (const auto& action : move.mr) {
-        auto action_index = action.cell * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
-        total_score += (map[action_index].first == 0) ? default_value : (static_cast<double>(map[action_index].second) / map[action_index].first);
-        size++;
+        auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
+        total_score += map[action_index].second;
+        weight_sum += map[action_index].first;
     }
-    return total_score / size;
+    return (weight_sum == 0) ? default_value : (static_cast<double>(total_score) / weight_sum);
 }
