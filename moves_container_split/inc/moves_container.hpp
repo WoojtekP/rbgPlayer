1eea83701a26de7f3207c0b8d062ca436cedc9c1
WoojtekP <pawlik.wj@gmail.com>
2020-08-18 23:09:05 +0200

    mastsplit with weighted_mean parameter

--- moves_container_split/inc/moves_container.hpp
+++ moves_container_split/inc/moves_container.hpp
@@ -3,23 +3,69 @@
 
 #include "reasoner.hpp"
 
+struct score {
+    double sum;
+    double weight;
+};
+
 class moves_container {
 private:
-    static constexpr uint size = reasoner::BOARD_SIZE * reasoner::NUMBER_OF_MODIFIERS;
-    std::pair<double, double> map[moves_container::size];
+    static constexpr uint size1 = reasoner::BOARD_SIZE * reasoner::NUMBER_OF_MODIFIERS;
+    static constexpr uint size2 = reasoner::BOARD_SIZE * reasoner::AUTOMATON_SIZE;
+    score map1[moves_container::size1];
+    score map2[moves_container::size2];
     void insert_or_update(const reasoner::move_representation&, const uint, const uint);
-    double get_score_or_default_value(const reasoner::move_representation&);
 public:
     moves_container();
     template <typename T>
-    void insert_or_update(const T& move, const uint score, [[maybe_unused]] const uint depth) {
+    void insert_or_update(const T& semimove, const uint score, const uint depth) {
+        insert_or_update(semimove.mr, score, depth);
+        auto index = (semimove.cell - 1) * reasoner::AUTOMATON_SIZE + semimove.state;
+        if constexpr (WEIGHT_SCALING) {
+            map2[index].weight += 1.0 / depth;
+            map2[index].sum += static_cast<double>(score) / depth;
+        }
+        else {
+            map2[index].weight += 1.0;
+            map2[index].sum += static_cast<double>(score);
+        }
+    }
+    void insert_or_update(const reasoner::move& move, const uint score, const uint depth) {
         insert_or_update(move.mr, score, depth);
     }
 
     template <typename T>
-    double get_score_or_default_value(const T& move) {
-        return get_score_or_default_value(move.mr);
+    double get_score_or_default_value(const T& semimove) {
+        double total_sum = 0;
+        [[maybe_unused]] double weight_sum = 0;
+        for (const auto& action : semimove.mr) {
+            auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
+            if (map1[action_index].weight == 0) {
+                return EXPECTED_MAX_SCORE;
+            }
+            if constexpr (WEIGHTED_MEAN) {
+                total_sum += map1[action_index].sum;
+                weight_sum += map1[action_index].weight;
+            }
+            else {
+                total_sum += map1[action_index].sum / map1[action_index].weight;
+            }
+        }
+        auto index = (semimove.cell - 1) * reasoner::AUTOMATON_SIZE + semimove.state;
+        if (map2[index].weight == 0) {
+            return EXPECTED_MAX_SCORE;
+        }
+        if constexpr (WEIGHTED_MEAN) {
+            total_sum += map2[index].sum;
+            weight_sum += map2[index].weight;
+            return total_sum / weight_sum;
+        }
+        else {
+            total_sum += map2[index].sum / map2[index].weight;
+            return total_sum / (1 + semimove.mr.size());
+        }
     }
+    double get_score_or_default_value(const reasoner::move&);
     void apply_decay_factor();
 };
 
--- moves_container_split/src/moves_container.cpp
+++ moves_container_split/src/moves_container.cpp
@@ -1,44 +1,61 @@
 #include "constants.hpp"
 #include "moves_container.hpp"
-
+#include "reasoner.hpp"
 
 moves_container::moves_container() {
-    for (uint i = 0; i < moves_container::size; ++i) {
-        map[i].first = map[i].second = 0.0;
+    for (uint i = 0; i < moves_container::size1; ++i) {
+        map1[i].weight = map1[i].sum = 0.0;
+    }
+    for (uint i = 0; i < moves_container::size2; ++i) {
+        map2[i].weight = map2[i].sum = 0.0;
     }
 }
 
-void moves_container::insert_or_update(const reasoner::move_representation& move, const uint score,[[maybe_unused]] const uint depth) {
-    for (const auto& action : move) {
+void moves_container::insert_or_update(const reasoner::move_representation& mr, const uint score, [[maybe_unused]] const uint depth) {
+    for (const auto& action : mr) {
         auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
-        if constexpr (WEIGHT_SCALING == 1) {
-            map[action_index].first += 1.0 / depth;
-            map[action_index].second += static_cast<double>(score) / depth;
+        if constexpr (WEIGHT_SCALING) {
+            map1[action_index].weight += 1.0 / depth;
+            map1[action_index].sum += static_cast<double>(score) / depth;
         }
         else {
-            map[action_index].first += 1.0;
-            map[action_index].second += static_cast<double>(score);
+            map1[action_index].weight += 1.0;
+            map1[action_index].sum += static_cast<double>(score);
         }
     }
 }
 
-double moves_container::get_score_or_default_value(const reasoner::move_representation& move) {
-    double weight_sum = 0;
-    double total_score = 0;
-    for (const auto& action : move) {
+double moves_container::get_score_or_default_value(const reasoner::move& move) {
+    double total_sum = 0;
+    [[maybe_unused]] double weight_sum = 0;
+    for (const auto& action : move.mr) {
         auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
-        if (map[action_index].first == 0) {
+        if (map1[action_index].weight == 0) {
             return EXPECTED_MAX_SCORE;
         }
-        total_score += map[action_index].second;
-        weight_sum += map[action_index].first;
+        if constexpr (WEIGHTED_MEAN) {
+            total_sum += map1[action_index].sum;
+            weight_sum += map1[action_index].weight;
+        }
+        else {
+            total_sum += map1[action_index].sum / map1[action_index].weight;
+        }
     }
-    return total_score / weight_sum;
+    if constexpr (WEIGHTED_MEAN) {
+        return total_sum / weight_sum;
+    }
+    else {
+        return total_sum / move.mr.size();
+    }
 }
 
 void moves_container::apply_decay_factor() {
-    for (uint i = 0; i < moves_container::size; ++i) {
-        map[i].first *= DECAY_FACTOR;
-        map[i].second *= DECAY_FACTOR;
+    for (uint i = 0; i < moves_container::size1; ++i) {
+        map1[i].weight *= DECAY_FACTOR;
+        map1[i].sum *= DECAY_FACTOR;
+    }
+    for (uint i = 0; i < moves_container::size2; ++i) {
+        map2[i].weight *= DECAY_FACTOR;
+        map2[i].sum *= DECAY_FACTOR;
     }
 }
