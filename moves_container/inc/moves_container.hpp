7e33b3f09ddc4c431f1893112cc61f5b4bdbc010
MSZ <marek.esz@gmail.com>
2020-10-28 13:55:42 +0100

    Custom hashmap.

--- moves_container/inc/moves_container.hpp
+++ moves_container/inc/moves_container.hpp
@@ -2,18 +2,49 @@
 #define MOVESCONTAINER
 
 #include <unordered_map>
-
-
 #include "reasoner.hpp"
 
+constexpr uint HASH_PRIMES[] = {23U,53U,97U,193U,389U,769U,1543U,3079U,6151U,12289U,24593U,49157U,98317U,196613U,393241U,786433U,1572869U,3145739U,6291469U,12582917U,25165843U,50331653U,100663319U,201326611U,402653189U,805306457U,1610612741U,3221225827U};
+constexpr uint HASH_INITIAL_LEVEL = 5;
+constexpr uint HASH_OVERLOAD_FACTOR = 2;
 
-class MovesContainer {
 struct move_hash {
     std::size_t operator()(const reasoner::move& move) const noexcept;
 };
+
+struct Entry {
+    reasoner::move mv;
+    double scores, weight;
+    uint next;
+    Entry(const reasoner::move _mv, double _scores, double _weight): mv(_mv), scores(_scores), weight(_weight), next(0) {}
+};
+
+class MovesContainer {
 private:
     std::unordered_map<reasoner::move, std::pair<double, double>, move_hash> map;
+    
+    uint capacity_level, capacity;
+    uint *hashtable;
+    std::vector<Entry> buckets;
+    
+    bool insert_or_update(const reasoner::move &move, const double score, const double weight);
+    void grow_hashtable();
+    
+   
 public:
+    static long unsigned int hash(const reasoner::move &move) noexcept;
+
+    MovesContainer():
+        capacity_level(HASH_INITIAL_LEVEL), capacity(HASH_PRIMES[capacity_level]),
+        hashtable(new uint[capacity]) {
+        for (uint i = 0; i < capacity; i++) hashtable[i] = 0;
+        buckets.emplace_back(reasoner::move(), 0.0, 0.0);
+    }
+
+    MovesContainer(const MovesContainer&) = delete;
+    void operator=(const MovesContainer&) = delete;
+    void operator=(const MovesContainer&&) = delete;
+    
     int insert_or_update(const reasoner::move& move, const uint score, const uint, const int);
     double get_score_or_default_value(const reasoner::move& move, const int);
     void apply_decay_factor();
--- moves_container/src/moves_container.cpp
+++ moves_container/src/moves_container.cpp
@@ -2,46 +2,100 @@
 
 #include <unordered_map>
 
-#include <boost/functional/hash.hpp>
-
 #include "constants.hpp"
 
+#include <iostream>
 
-std::size_t MovesContainer::move_hash::operator()(const reasoner::move& move) const noexcept {
-    std::size_t seed = 0;
-    for (const auto& action : move.mr) {
-        //boost::hash_combine(seed, action.index);
-        //boost::hash_combine(seed, action.cell);
-        seed = (seed * 13 + action.index) * 17 + action.cell;
+void MovesContainer::grow_hashtable() {
+    //std::cout << "growing hashtable from " << capacity << " to " << HASH_PRIMES[capacity_level+1] << ", size " << buckets.size() << "\n";
+    delete[] hashtable;
+    capacity = HASH_PRIMES[++capacity_level];
+    hashtable = new uint[capacity];
+    for (uint i = 0; i < capacity; i++) hashtable[i] = 0;
+    for (uint i = 1; i < buckets.size(); i++) {
+        buckets[i].next = 0;
+        uint hashindex = hash(buckets[i].mv) % capacity;
+        uint index = hashtable[hashindex];
+        if (index == 0) {
+            hashtable[hashindex] = i;
+        } else {
+            while (buckets[index].next) index = buckets[index].next;
+            buckets[index].next = i;
+        }
     }
-    return seed;
+}
+
+std::size_t move_hash::operator()(const reasoner::move& move) const noexcept {
+    return MovesContainer::hash(move);
+}
+
+long unsigned int MovesContainer::hash(const reasoner::move& move) noexcept {
+    long unsigned int hash = 0;
+    for (const auto& action : move.mr) {
+       hash = (hash * 13 + action.index) * 17 + action.cell;
+    }
+    return hash;
 }
 
 int MovesContainer::insert_or_update(const reasoner::move& move, const uint score, const uint, const int) {
+    uint hashindex = hash(move) % capacity;
+    uint index = hashtable[hashindex];
+    bool found = false;
+    if (index == 0) {
+        hashtable[hashindex] = buckets.size();
+        buckets.emplace_back(move, score, 1.0);
+    } else {
+        uint collisions = 0;
+        while (true) {
+            if (buckets[index].mv == move) {
+                buckets[index].scores += score;
+                buckets[index].weight += 1.0;
+                found = true;
+                break;
+            }
+            if (buckets[index].next == 0) {
+                buckets[index].next = buckets.size();
+                buckets.emplace_back(move, score, 1.0);
+                break;
+            }
+            index = buckets[index].next;
+            collisions++;
+        }
+    }
+    if (buckets.size()*HASH_OVERLOAD_FACTOR >= capacity) grow_hashtable();
+    /*
+    // Uncomment to test
     auto it = map.find(move);
     if (it == map.end()) {
         map.insert({move, {1.0, static_cast<double>(score)}});
+        if (found) {
+            std::cout << "err found\n";
+        }
     }
     else {
         ++(it->second.first);
         it->second.second += static_cast<double>(score);
-    }
+        if (!found) std::cout << "err not found\n";
+    }*/
     return 0;
 }
 
 double MovesContainer::get_score_or_default_value(const reasoner::move& move, const int) {
-    auto it = map.find(move);
-    if (it == map.end()) {
-        return EXPECTED_MAX_SCORE;
+    uint hashindex = move_hash()(move) % capacity;
+    uint index = hashtable[hashindex];
+    bool found;
+    while (index != 0) {
+        if (buckets[index].mv == move)
+            return buckets[index].scores / buckets[index].weight;
+        index = buckets[index].next;
     }
-    else {
-        return it->second.second / it->second.first;
-    }
+    return EXPECTED_MAX_SCORE;
 }
 
 void MovesContainer::apply_decay_factor() {
-    for (auto& el : map) {
-        el.second.first *= DECAY_FACTOR;
-        el.second.second *= DECAY_FACTOR;
+    for (uint i = 1; i < buckets.size(); i++) {
+        buckets[i].scores *= DECAY_FACTOR;
+        buckets[i].weight *= DECAY_FACTOR;
     }
+    //std::cout << "load factor " << (double)buckets.size()/capacity << "\n";
 }
