6fb3b42bbd72b62390da887c5e8458a427df06ea
WoojtekP <pawlik.wj@gmail.com>
2020-12-03 23:49:48 +0100

    adaptation of mastsplit

--- actions_arrays/inc/actions_arrays.hpp
+++ actions_arrays/inc/actions_arrays.hpp
@@ -8,50 +8,18 @@
 class ActionsArrays {
 private:
     struct score {
-        double sum;
-        double weight;
+        double sum = 0;
+        double weight = 0;
+        score() = default;
     };
-    static constexpr uint size1 = ONLY_STATES ? 1 : reasoner::BOARD_SIZE * reasoner::NUMBER_OF_MODIFIERS;
-    static constexpr uint size2 = reasoner::BOARD_SIZE * reasoner::AUTOMATON_SIZE;
-    score arr1[ActionsArrays::size1];
-    score arr2[ActionsArrays::size2];
-    void insert_or_update(const reasoner::move_representation&, const double, const double);
-
+    static constexpr uint size = reasoner::BOARD_SIZE * (reasoner::NUMBER_OF_MODIFIERS + reasoner::AUTOMATON_SIZE);
+    score arr[ActionsArrays::size];
+    uint action_to_index(const reasoner::action_representation);
 public:
     ActionsArrays();
-    template <typename T>
-    int insert_or_update(const T& semimove, const double score, const double weight) {
-        if constexpr (!ONLY_STATES) {
-            insert_or_update(semimove.mr, score, weight);
-        }
-        auto index = (semimove.cell - 1) * reasoner::AUTOMATON_SIZE + semimove.state;
-        arr2[index].weight += weight;
-        arr2[index].sum += score;
-        return 0;
-    }
+    int insert_or_update(const reasoner::action_representation, const double, const double);
     int insert_or_update(const reasoner::move&, const double, const double);
-
-    template <typename T>
-    double get_score_or_default_value(const T& semimove) {
-        const auto index = (semimove.cell - 1) * reasoner::AUTOMATON_SIZE + semimove.state;
-        if constexpr (ONLY_STATES) {
-            return arr2[index].sum / arr2[index].weight;
-        }
-        double total_sum = 0;
-        [[maybe_unused]] double weight_sum = 0;
-        for (const auto& action : semimove.mr) {
-            auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
-            if (arr1[action_index].weight == 0) {
-                return EXPECTED_MAX_SCORE;
-            }
-            total_sum += arr1[action_index].sum / arr1[action_index].weight;
-        }
-        if (arr2[index].weight == 0) {
-            return EXPECTED_MAX_SCORE;
-        }
-        total_sum += arr2[index].sum / arr2[index].weight;
-        return total_sum / (1 + semimove.mr.size());
-    }
+    double get_score_or_default_value(const reasoner::action_representation );
     double get_score_or_default_value(const reasoner::move&);
     void apply_decay_factor();
 };
--- actions_arrays/src/actions_arrays.cpp
+++ actions_arrays/src/actions_arrays.cpp
@@ -3,51 +3,51 @@
 #include "reasoner.hpp"
 
 ActionsArrays::ActionsArrays() {
-    for (uint i = 0; i < ActionsArrays::size1; ++i) {
-        arr1[i].weight = arr1[i].sum = 0.0;
-    }
-    for (uint i = 0; i < ActionsArrays::size2; ++i) {
-        arr2[i].weight = arr2[i].sum = 0.0;
+    for (uint i = 0; i < ActionsArrays::size; ++i) {
+        arr[i].sum = arr[i].weight = 0.0;
     }
 }
 
-void ActionsArrays::insert_or_update(const reasoner::move_representation& mr, const double score, const double weight) {
-    for (const auto& action : mr) {
-        auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
-        arr1[action_index].weight += weight;
-        arr1[action_index].sum += score;
-    }
+uint ActionsArrays::action_to_index(const reasoner::action_representation action) {
+    return (action.cell - 1) * (reasoner::NUMBER_OF_MODIFIERS + reasoner::AUTOMATON_SIZE) + reasoner::AUTOMATON_SIZE + action.index - 1;
+}
+
+int ActionsArrays::insert_or_update(const reasoner::action_representation action, const double score, const double weight) {
+    auto index = action_to_index(action);
+    arr[index].weight += weight;
+    arr[index].sum += score;
+    return 0;
 }
 
 int ActionsArrays::insert_or_update(const reasoner::move& move, const double score, const double weight) {
-    if constexpr (!ONLY_STATES) {
-        insert_or_update(move.mr, score, weight);
+    for (const auto& action : move.mr) {
+        auto index = action_to_index(action);
+        arr[index].weight += weight;
+        arr[index].sum += score;
     }
     return 0;
 }
 
+double ActionsArrays::get_score_or_default_value(const reasoner::action_representation action) {
+    const auto index = action_to_index(action);
+    return arr[index].weight == 0 ? EXPECTED_MAX_SCORE : arr[index].sum / arr[index].weight;
+}
+
 double ActionsArrays::get_score_or_default_value(const reasoner::move& move) {
-    double total_sum = 0;
-    [[maybe_unused]] double weight_sum = 0;
+    double sum = 0;
     for (const auto& action : move.mr) {
-        auto action_index = (action.cell - 1) * reasoner::NUMBER_OF_MODIFIERS + reasoner::action_to_modifier_index(action.index);
-        if (arr1[action_index].weight == 0) {
+        auto index = action_to_index(action);
+        if (arr[index].weight == 0) {
             return EXPECTED_MAX_SCORE;
         }
-        total_sum += arr1[action_index].sum / arr1[action_index].weight;
+        sum += arr[index].sum / arr[index].weight;
     }
-    return total_sum / move.mr.size();
+    return sum / move.mr.size();
 }
 
 void ActionsArrays::apply_decay_factor() {
-    if constexpr (!ONLY_STATES) {
-        for (uint i = 0; i < ActionsArrays::size1; ++i) {
-            arr1[i].weight *= DECAY_FACTOR;
-            arr1[i].sum *= DECAY_FACTOR;
-        }
-    }
-    for (uint i = 0; i < ActionsArrays::size2; ++i) {
-        arr2[i].weight *= DECAY_FACTOR;
-        arr2[i].sum *= DECAY_FACTOR;
+    for (uint i = 0; i < ActionsArrays::size; ++i) {
+        arr[i].weight *= DECAY_FACTOR;
+        arr[i].sum *= DECAY_FACTOR;
     }
 }
