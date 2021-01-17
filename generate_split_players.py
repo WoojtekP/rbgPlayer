144a1ff170a217c4d0d37b9928b95497187db3f8
MSZ <marek.esz@gmail.com>
2021-01-17 21:56:41 +0100

    ModShift split strategy

--- generate_split_players.py
+++ generate_split_players.py
@@ -29,6 +29,7 @@ for agent in get_agents():
     M_agent = "agents/mctsM_" + agent + ".json"
     MP_agent = "agents/mctsMP_" + agent + ".json"
     MPS_agent = "agents/mctsMPS_" + agent + ".json"
+    MS_agent = "agents/mctsMS_" + agent + ".json"
     if not os.path.isfile(M_agent):
         print("error: ", agent, "does not exist - skipped")
         continue
@@ -42,3 +43,7 @@ for agent in get_agents():
         with open(MPS_agent, "w") as mps_config_file:
             json.dump(agent_config, mps_config_file, indent=4)
             print(agent, "MPS created successfully")
+        agent_config["algorithm"]["split_strategy"] = "ModShift"
+        with open(MS_agent, "w") as ms_config_file:
+            json.dump(agent_config, ms_config_file, indent=4)
+            print(agent, "MS created successfully")
